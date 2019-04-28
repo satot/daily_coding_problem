23760e8b98cb03b7d510306b4de1f0bc19f1332f
satot <ph1.ph2.sa25@gmail.com>
2019-04-29 00:41:55 +0800

    Solved #5

---
+++ 5.py
@@ -0,0 +1,18 @@
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+
+def car(pair):
+    def first(a, b):
+        return a
+    return pair(first)
+
+def cdr(pair):
+    def last(a, b):
+        return b
+    return pair(last)
+
+
+assert car(cons(3, 4)) == 3
+assert cdr(cons(3, 4)) == 4
