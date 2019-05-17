a08f1b50ac4ab1b10ad9141090e728a889ada045
satot <ph1.ph2.sa25@gmail.com>
2019-05-18 00:14:07 +0800

    Learning back_tracking approach

---
+++ back_tracking.py
@@ -0,0 +1,40 @@
+# Pattern 1
+def n_queens(n, board=[]):
+    if n == len(board):
+        return 1
+
+    count = 0
+    for col in range(n):
+        board.append(col)
+        if is_valid(board):
+            count += n_queens(n, board)
+        board.pop()
+    return count
+
+def is_valid(board):
+    current_queen_row, current_queen_col = len(board) - 1, board[-1]
+    # Check if any queens can attack the last queen.
+    for row, col in enumerate(board[:-1]):
+        diff = abs(current_queen_col - col)
+        if diff == 0 or diff == current_queen_row - row:
+            return False
+    return True
+
+for i in range(10):
+    print n_queens(i)
+
+
+# Pattern 2
+def get_itinerary(flights, current_itinerary):
+    # If we've used up all the flights, we're done
+    if not flights:
+        return current_itinerary
+    last_stop = current_itinerary[-1]
+    for i, (origin, destination) in enumerate(flights):
+        # Make a copy of flights without the current one to mark it as used
+        flights_minus_current = flights[:i] + flights[i + 1:]
+        current_itinerary.append(destination)
+        if origin == last_stop:
+            return get_itinerary(flights_minus_current, current_itinerary)
+        current_itinerary.pop()
+    return None
